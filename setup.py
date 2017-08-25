023157253254a38b2130eb93ef229d1f6db85cda
Alex Lubbock <code@alexlubbock.com>
2017-08-25 12:51:06 -0500

    Minor edits to setup.py for package building

--- setup.py
+++ setup.py
@@ -11,9 +11,10 @@ def main():
           author_email='code@alexlubbock.com',
           packages=['pydrc'],
           install_requires=['numpy', 'scipy', 'pandas', 'plotly', 'seaborn'],
-          test_requires=['nose', 'nosebook'],
+          tests_require=['nose', 'nosebook'],
           test_suite='nose.collector',
-          cmdclass=versioneer.get_cmdclass()
+          cmdclass=versioneer.get_cmdclass(),
+          zip_safe=True
     )
 
 if __name__ == '__main__':
