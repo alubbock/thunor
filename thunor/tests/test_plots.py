5ffc0e6e55cd643dd8bb0d93edbd25bdce85ca69
Alex Lubbock <code@alexlubbock.com>
2018-04-28 21:01:01 -0500

    Improve test coverage

--- thunor/tests/test_plots.py
+++ thunor/tests/test_plots.py
@@ -5,7 +5,7 @@ import thunor.dip
 import thunor.viability
 import thunor.curve_fit
 from thunor.plots import plot_time_course, plot_drc, plot_drc_params, \
-    plot_plate_map
+    plot_plate_map, plot_ctrl_dip_by_plate
 from thunor.helpers import plotly_to_dataframe
 import pandas as pd
 
@@ -17,6 +17,7 @@ class TestWithDataset(unittest.TestCase):
                                                        'testdata/hts007.h5')
         cls.dataset = thunor.io.read_hdf(cls.filename)
         ctrl_dip_data, expt_dip_data = thunor.dip.dip_rates(cls.dataset)
+        cls.ctrl_dip_data = ctrl_dip_data
 
         cls.fit_params = thunor.curve_fit.fit_params(ctrl_dip_data,
                                                      expt_dip_data)
@@ -24,6 +25,7 @@ class TestWithDataset(unittest.TestCase):
         viability_data, _ = thunor.viability.viability(
             cls.dataset, include_controls=False)
         cls.viability_params = thunor.curve_fit.fit_params(
+            fit_cls=thunor.curve_fit.HillCurveLL3u,
             ctrl_data=None, expt_data=viability_data)
 
     def test_plot_viability_curves(self):
@@ -47,7 +49,7 @@ class TestWithDataset(unittest.TestCase):
 
         df._drmetric = self.fit_params._drmetric
 
-        plot_drc_params(df, fit_param='auc', multi_dataset=True)
+        plot_drc_params(df, fit_param='ec50', multi_dataset=True)
 
     def test_plot_time_course(self):
         abe_bt20 = self.dataset.filter(drugs=['abemaciclib'],
@@ -58,6 +60,11 @@ class TestWithDataset(unittest.TestCase):
 
     def test_plot_dip(self):
         assert isinstance(plotly_to_dataframe(plot_drc(self.fit_params)),
+                          pd.DataFrame)
+
+    def test_plot_dip_single_curve(self):
+        fit_data = self.fit_params.iloc[[0], :]
+        assert isinstance(plotly_to_dataframe(plot_drc(fit_data)),
                           pd.DataFrame)
 
     def test_plot_dip_params_single_param(self):
@@ -79,6 +86,10 @@ class TestWithDataset(unittest.TestCase):
             aggregate_drugs={'tag': ['abemaciclib', 'Panobinostat']},
             fit_param='ic50')),
                           pd.DataFrame)
+
+    def test_plot_ctrl_dip_by_plate(self):
+        res = plot_ctrl_dip_by_plate(self.ctrl_dip_data)
+        assert isinstance(plotly_to_dataframe(res), pd.DataFrame)
 
     def test_plot_plate_map(self):
         plate_data = self.dataset.plate('HTS007_149-28A',
